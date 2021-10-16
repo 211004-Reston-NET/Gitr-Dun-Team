bd30565ca074806f13804080c41614c5231d9a39
ccwilki1 <ccwilki1@gmail.com>
2021-10-15 22:34:26 -0400

    second commit

---
+++ CRUSMOD/Clothes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CRUSMOD
+{
+    public class Clothes
+    {
+        private String _city;
+        public String City
+        {
+            get { return _city; }
+            set 
+           { if (!Regex.IsMatch(value, @"[A-Za-z .]+$")) 
+            throw new Exception("City can only hold letters");
+            _city = value;
+        }
+            
+            
+        }
+        
+    }
+}
